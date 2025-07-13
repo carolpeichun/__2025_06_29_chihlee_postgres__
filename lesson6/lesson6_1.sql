ebcf7d79dfc59e5adfd6c9239581a15fb74a75ef
Carol <123@gmail.com>
2025-07-13 07:22:50 +0000

    update lesson6 3rd

--- lesson6/lesson6_1.sql
+++ lesson6/lesson6_1.sql
@@ -2,3 +2,6 @@ ALTER TABLE "台鐵車站資訊" ADD PRIMARY KEY("stationCode");
 
 ALTER TABLE "每日各站進出站人數" ADD FOREIGN KEY("車站代碼")
 REFERENCES "台鐵車站資訊"("stationCode") ON DELETE SET NULL;
+
+SELECT count(*) AS "總筆數"
+FROM "每日各站進出站人數";
