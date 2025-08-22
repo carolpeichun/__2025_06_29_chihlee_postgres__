3f038b0947a98421ae3fae9ac34960178f6f9ddf
CarolStudy <123@gmail.com>
2025-08-22 10:22:05 +0000

    update lesson11_1.sql

--- lesson11/lesson11_1.sql
+++ lesson11/lesson11_1.sql
@@ -12,13 +12,14 @@ WHERE s."stationName" = '基隆'
 SELECT MIN("日期") AS min_date, MAX("日期") AS max_date
         FROM public."每日各站進出站人數";
 
-SELECT p."日期",
-       t."stationName" AS 車站,
-       p."進站人數",
-       p."出站人數"
-FROM public."每日各站進出站人數" p
-JOIN public."台鐵車站資訊" t
-  ON p."車站代碼" = t."stationCode"
-WHERE p."日期" BETWEEN DATE '2023-01-01' AND DATE '2023-01-31'
-  AND t."stationName" = '基隆'
-ORDER BY p."日期";
+SELECT
+  d."日期"        AS date,
+  s."stationName" AS station,
+  d."進站人數"    AS in_count,
+  d."出站人數"    AS out_count
+FROM "每日各站進出站人數" AS d
+JOIN "台鐵車站資訊" AS s
+  ON d."車站代碼" = s."stationCode"
+WHERE s."stationName" = '基隆'
+  AND d."日期" BETWEEN '2023-01-01' AND '2023-01-31'
+ORDER BY d."日期";
